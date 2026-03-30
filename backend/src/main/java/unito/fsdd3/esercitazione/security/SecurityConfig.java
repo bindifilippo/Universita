d57dc06cda068320e166958512eca3da6e6968a5
Filippo Bindi <192812197+bindifilippo@users.noreply.github.com>
2026-03-30 11:51:06 +0200

    add CORS configuration and security filter chain; update database URL

--- backend/src/main/java/unito/fsdd3/esercitazione/security/SecurityConfig.java
+++ backend/src/main/java/unito/fsdd3/esercitazione/security/SecurityConfig.java
@@ -1,5 +1,24 @@
 package unito.fsdd3.esercitazione.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
-    
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+        
+    } 
 }
--- backend/src/main/java/unito/fsdd3/esercitazione/security/WebConfiguration.java
+++ backend/src/main/java/unito/fsdd3/esercitazione/security/WebConfiguration.java
@@ -1,4 +1,4 @@
-package unito.fsdd3.esercitazione.config;
+package unito.fsdd3.esercitazione.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,18 +6,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+class WebConfiguration {
+
+    private static final String frontEndUrl = "http://localhost:5173";
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins(frontEndUrl)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
